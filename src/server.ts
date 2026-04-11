671a7ebf931b2a1f54784a013c5202623524579e
Renzo Barrueta <renzobarrueta@gmail.com>
2026-04-11 18:35:14 -0400

    fix port render

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from './app.ts'
+import app from './app'
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT || 10000
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en el puerto:${PORT}`)
